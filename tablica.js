458b0edea7a25384e5cd88b3c4dc17fee40bd6b2
m-be-k <me.madik@mail.ru>
2024-09-28 16:46:53 +0600

    test

--- tablica.js
+++ tablica.js
@@ -1,31 +1,19 @@
-const taible = document.getElementById("tablica");
+const val = document.getElementById("tablica");
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
         const div = document.createElement('div')
         div.textContent = i * j;
-        taible.append(div);
-        // div.setAttribute('class', 'grid-stile');
-        // if (i === j) {
-        //     div.setAttribute('class', 'diagonal');
-        // }
-        // if (i === 1 || j === 1) {
-        //     div.setAttribute('class', 'blue');
-        // }
-        // if( i>j){
-        //     div.setAttribute('class','red');
-        //
-        // }
-        if (i === 1||j===1) {
-            div.classList.add('diagonal');
-        }
-        else if(i===j){
+        div.classList.add('grid-style');
+        val.append(div);
+
+        if (i === 1 || j === 1) {
+            div.classList.add('blue-diagonal');
+        } else if (i === j) {
+            div.classList.add('green');
+        } else if (i > j) {
             div.classList.add('blue');
-        }
-        else if (i>j){
-            div.classList.add('red');
-        }
-        else {
-            div.classList.add('orange');
+        } else {
+            div.classList.add('cabbage');
         }
     }
 }
