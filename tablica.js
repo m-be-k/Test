aa207d25696e85c19018aa0fd3144ad59eedeea0
m-be-k <me.madik@mail.ru>
2024-09-28 16:50:03 +0600

    test

--- tablica.js
+++ tablica.js
@@ -1,4 +1,4 @@
-const val = document.getElementById("tablica");
+const val = document.getElementById("table");
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
         const div = document.createElement('div')
