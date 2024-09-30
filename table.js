f90a171e5f0289141255829bd81f08c9153d0f82
m-be-k <me.madik@mail.ru>
2024-09-30 21:31:09 +0600

    изменение цвета по таблице с помощью клика.

--- table.js
+++ table.js
@@ -1,11 +1,16 @@
 const val = document.getElementById("table");
+
+//mouseenter - вхождение мышки в елемент
+//mouseleave - выход мышки из элемента
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
         const div = document.createElement('div')
         div.textContent = i * j;
         div.classList.add('grid-style');
         val.append(div);
-
+        div.onclick = () => {
+            div.classList.add('white');
+        }
         if (i === 1 || j === 1) {
             div.classList.add('blue-diagonal');
         } else if (i === j) {
